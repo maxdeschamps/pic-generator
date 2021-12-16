ac7ce58c61091dc564041b53bce87e0b81801c4b
Maxence Deschamps <maxence.deschamps.pro@gmail.com>
2021-12-16 11:14:59 +0100

    :card_file_box: ajout des modèles de données

---
+++ src/main/java/com/picgenerator/entities/Image.java
@@ -0,0 +1,32 @@
+package com.picgenerator.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "image")
+public class Image implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="src")
+    private String src;
+
+    @ManyToMany(mappedBy = "images")
+    @JsonIgnore
+    private List<Tag> tags;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    @JsonIgnore
+    private User author;
+}
---
+++ src/main/java/com/picgenerator/entities/User.java
@@ -0,0 +1,36 @@
+package com.picgenerator.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "tag")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="firstname")
+    private String firstname;
+
+    @Column(name="lastname")
+    private String lastname;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="role")
+    private Role role;
+
+    @OneToMany(mappedBy = "author")
+    @JsonIgnore
+    private List<Image> images;
+}
