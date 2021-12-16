06b16c3f1ebf51101f896abe65bdc3b6d7bf371d
Maxence Deschamps <maxence.deschamps.pro@gmail.com>
2021-12-16 12:36:28 +0100

    :sparkles: ajout des services

---
+++ src/main/java/com/picgenerator/services/ImageService.java
@@ -0,0 +1,13 @@
+package com.picgenerator.services;
+
+import com.picgenerator.repositories.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+}
