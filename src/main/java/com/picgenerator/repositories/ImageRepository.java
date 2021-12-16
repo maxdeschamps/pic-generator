2833fecc21fa30da5f3a07c7a654720ac52a4f0c
Maxence Deschamps <maxence.deschamps.pro@gmail.com>
2021-12-16 12:36:48 +0100

    :sparkles: ajout des repositories

---
+++ src/main/java/com/picgenerator/repositories/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.picgenerator.repositories;
+
+import com.picgenerator.entities.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Integer> {
+}
---
+++ src/main/java/com/picgenerator/repositories/TagRepository.java
@@ -0,0 +1,9 @@
+package com.picgenerator.repositories;
+
+import com.picgenerator.entities.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+}
