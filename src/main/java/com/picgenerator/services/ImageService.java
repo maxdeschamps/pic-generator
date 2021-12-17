1bb968e8a66488548b60f2197359789a94ad9df1
Maxence Deschamps <maxence.deschamps.pro@gmail.com>
2021-12-17 09:54:10 +0100

    :sparkles: ajout des Service et Controller de Image

--- src/main/java/com/picgenerator/services/ImageService.java
+++ src/main/java/com/picgenerator/services/ImageService.java
@@ -1,13 +1,32 @@
 package com.picgenerator.services;
 
+import com.picgenerator.entities.Image;
 import com.picgenerator.repositories.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ImageService {
 
     @Autowired
     private ImageRepository imageRepository;
+
+    public Image getById(Integer id) {
+        return imageRepository.findById(id).orElse(null);
+    }
+
+    public Image createOrUpdate(Image image) {
+        return imageRepository.save(image);
+    }
+
+    public List<Image> getAll() {
+        return imageRepository.findAll();
+    }
+
+    public void deleteById(Integer id) {
+        imageRepository.delete(getById(id));
+    }
 
 }
