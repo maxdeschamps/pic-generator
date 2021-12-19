5a385d4e47f951b8c9f2230efe7c01cf3b5b6d7f
Jean-Philippe Bourdais <jeanphi.bourdais@gmail.com>
2021-12-19 18:33:06 +0100

    ✨ add im4java, generate img

--- src/main/java/com/picgenerator/controllers/ImageController.java
+++ src/main/java/com/picgenerator/controllers/ImageController.java
@@ -1,7 +1,48 @@
 package com.picgenerator.controllers;
 
-public class ImageController {
+import com.picgenerator.services.ImageService;
+import org.im4java.core.IM4JavaException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-    
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+
+@RestController
+public class ImageController {
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @Autowired
+    ImageService imageService;
+
+    @GetMapping("/test")
+    public ResponseEntity<Resource> Index() throws IOException, InterruptedException, IM4JavaException {
+
+        BufferedImage image = ImageIO.read(this.getClass().getResourceAsStream("/assets/img/noel_1.jpg"));
+        image = imageService.imageProcessing(image);
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", os);
+        InputStream systemResourceAsStream = new ByteArrayInputStream(os.toByteArray());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("image/jpeg"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                .body(new InputStreamResource(systemResourceAsStream));
+        //URL resourceUrl = this.getClass().getClassLoader().getResource("/assets/img/noel_1.jpg")
+    }
 
 }
--- src/main/java/com/picgenerator/services/ImageService.java
+++ src/main/java/com/picgenerator/services/ImageService.java
@@ -1,8 +1,16 @@
 package com.picgenerator.services;
 
 import com.picgenerator.repositories.ImageRepository;
+import org.im4java.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
 
 @Service
 public class ImageService {
@@ -10,4 +18,25 @@ public class ImageService {
     @Autowired
     private ImageRepository imageRepository;
 
+    public BufferedImage imageProcessing(BufferedImage image) throws IOException, InterruptedException, IM4JavaException {
+
+        String imPath="C:\\Program Files\\ImageMagick";
+
+        IMOperation op = new IMOperation();
+        op.addImage();
+        op.colorize(30, 40, 0);
+        op.addImage("jpg:-");
+
+        // set up command
+        ConvertCmd convert = new ConvertCmd();
+        convert.setSearchPath(imPath);
+        Stream2BufferedImage s2b = new Stream2BufferedImage();
+        convert.setOutputConsumer(s2b);
+
+        // run command and extract BufferedImage from OutputConsumer
+        convert.run(op,image);
+        BufferedImage convertImage = s2b.getImage();
+
+        return convertImage;
+    }
 }
