1e4b054ebdb9737c108dfc299c483a62f8f24c38
Antoine Bonin <antoine.bonin@ynov.com>
2022-01-04 13:46:23 +0100

    ✅ create image test for crop and resizing

--- src/test/java/com/picgenerator/controllers/ImageControllerTest.java
+++ src/test/java/com/picgenerator/controllers/ImageControllerTest.java
@@ -1,24 +1,80 @@
 package com.picgenerator.controllers;
 
-import com.picgenerator.RequestUtility;
+import com.picgenerator.utils.ImageUtility;
+import com.picgenerator.utils.RequestUtility;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 class ImageControllerTest {
 
     @Autowired
     RequestUtility requestUtility = new RequestUtility();
+    @Autowired
+    ImageUtility imageUtility = new ImageUtility();
 
     @Test
-    void getImage() throws IOException {
-        requestUtility.getImageEndpoint("image?id=1");
+    @DisplayName("Get image by ID")
+    void getImageById() throws IOException {
+        requestUtility.getJsonEndpoint("image?id=1");
     }
 
     @Test
+    @DisplayName("Get image by Tag")
+    void getImageByTagId() throws IOException {
+        requestUtility.getImageEndpoint("generate?t=1");
+    }
+
+    @Nested
+    @DisplayName("Generate image")
+    class Generate {
+
+        @Test
+        @DisplayName("No option")
+        void generateImage() throws  IOException {
+            requestUtility.getImageEndpoint("generate");
+        }
+
+        @Test
+        @DisplayName("Width Height")
+        void generateImageWidthHeight() throws  IOException {
+            Integer width = 500;
+            Integer height = 500;
+            String endpoint = "generate?w=" + width + "&h=" + height;
+            requestUtility.getImageEndpoint(endpoint);
+            BufferedImage img = imageUtility.getImageFromResponse(endpoint);
+            assertThat(width, equalTo(img.getWidth()));
+            assertThat(height, equalTo(img.getHeight()));
+        }
+
+        @Test
+        @DisplayName("Width Height Resize")
+        void generateImageWidthHeightResize() throws  IOException {
+            Integer width = 500;
+            Integer height = 500;
+            String endpoint = "generate?w=" + width + "&h=" + height + "&opt=2";
+            requestUtility.getImageEndpoint(endpoint);
+            BufferedImage img = imageUtility.getImageFromResponse(endpoint);
+
+            if (img.getWidth() == width || img.getHeight() == height) {
+                assertThat(true, equalTo(true));
+            } else {
+                assertThat(false, equalTo(true));
+            }
+        }
+
+    }
+
+
+    @Test
+    @DisplayName("Get all images")
     void getAllImage() throws IOException {
         requestUtility.getJsonEndpoint("images/all");
     }
--- src/test/java/com/picgenerator/utils/RequestUtility.java
+++ src/test/java/com/picgenerator/utils/RequestUtility.java
@@ -1,4 +1,4 @@
-package com.picgenerator;
+package com.picgenerator.utils;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
